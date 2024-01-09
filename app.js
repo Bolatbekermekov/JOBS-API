d2d217ffc86e5e87346c6d4c37cc89a5b32f2b68
Bolatbekermekov <132038849+Bolatbekermekov@users.noreply.github.com>
2024-01-09 18:25:22 +0600

    Исправлен порт для запуска на Heroku

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(notFound)
 app.use(CustomAPIError)
 
 
-const port = 3000
+const port = process.env.PORT || 3000;
 const start = async()=>{
   try{ await connectDb(process.env.MONGO_URI)
     app.listen(port,()=>console.log(`app listening on port ${port}`))
